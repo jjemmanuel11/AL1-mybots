089865e6b1e105a3e45ea675f9f463cbbf7219a7
jjemmanuel11 <102261362+jjemmanuel11@users.noreply.github.com>
2023-01-08 17:11:26 -0600

    initial steps

--- generate.py
+++ generate.py
@@ -1,22 +1,16 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 
 x=0
-for i in range(5):
-    y=0
-    for j in range(5):
-        length=1
-        width=1
-        height=1
-        z=0
-        for k in range(0,10):
-            pyrosim.Send_Cube(name="Box", pos=[x ,y ,z] , size=[length,width,height])
-            z = z + height/2 + height *.9/2
-            length = length*0.9
-            width= width * 0.9
-            height= height * 0.9
-        y+=1
-    x+=1
-            
+y=0
+z=0.5
+
+length= 1
+width=1
+height=1
+
+pyrosim.Send_Cube(name="Box", pos=[x ,y ,z] , size=[length,width,height])
+
+
 pyrosim.End()
