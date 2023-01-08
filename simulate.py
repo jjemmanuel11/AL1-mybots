d174c175fee8f480cfc3402d53c95b463ad39760
jjemmanuel11 <102261362+jjemmanuel11@users.noreply.github.com>
2023-01-08 15:57:33 -0600

    playing w box sizes

--- simulate.py
+++ simulate.py
@@ -6,12 +6,14 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 planeId = p.loadURDF("plane.urdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/100)
     print(i)
 p.disconnect()
+
+
 
 
