74467a10ebb0bc69b9287856ae41627bbcb2d831
jjemmanuel11 <102261362+jjemmanuel11@users.noreply.github.com>
2023-01-15 21:31:19 -0600

    analyze

--- analyze.py
+++ analyze.py
@@ -1,1 +1,12 @@
-import numpy
+import numpy 
+import matplotlib.pyplot as mpb
+
+backpoints = numpy.load("data/backLegSensorValues.npy")
+frontpoints = numpy.load("data/FrontLegSensorValues.npy")
+
+mpb.plot(backpoints, label="Back Leg", linewidth = 4)
+mpb.plot(frontpoints, label="Front Leg", linewidth = 4)
+
+mpb.legend()
+mpb.show()
+
