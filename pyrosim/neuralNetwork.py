b7466081614a97f0bdf19cb2a2db212f08900c55
jjemmanuel11 <102261362+jjemmanuel11@users.noreply.github.com>
2023-01-22 20:03:57 -0600

    update

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -42,7 +42,11 @@ class NEURAL_NETWORK:
     def Is_Motor_Neuron(self, neuronName):
         return self.neurons[neuronName].Is_Motor_Neuron()
         
-
+    def Get_Motor_Neurons_Joint(self, neuronName):
+        return self.neurons[neuronName].Get_Joint_Name()
+    
+    def Get_Value_Of(self):
+        pass
 # ---------------- Private methods --------------------------------------
 
     def Add_Neuron_According_To(self,line):
--- robot.py
+++ robot.py
@@ -32,7 +32,11 @@ class ROBOT:
     def Act(self, time):
         for neuronName in self.nn.Get_Neuron_Names():
             if self.nn.Is_Motor_Neuron(neuronName):
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+                desiredAngle = self.nn.Get_Value_Of(neuronName)
                 print(neuronName)
+                print(jointName)
+
         for motor in self.motors:
             self.motors[motor].Set_Value(self.robotId, time)
 
